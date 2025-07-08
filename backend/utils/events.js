22920bd8cd17b13d046afb76dca71ca848774a35
yiwang0w0 <2949215486@qq.com>
2025-07-08 15:07:33 +0900

    Create events.js

---
+++ backend/utils/events.js
@@ -0,0 +1,8 @@
+// backend/utils/events.js
+
+module.exports = {
+  onPlayerMove: (player, game, room) => [],
+  onPlayerAttackNpc: (player, npc, game, room) => [],
+  onPlayerAttackPlayer: (player, target, game, room) => [],
+  resolveStatus: (player, game, room, log) => {},
+};
