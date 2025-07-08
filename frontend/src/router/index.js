d4388793889f0a58b71f392b75d2ac479d2051c4
yiwang0w0 <77381968+yiwang0w0@users.noreply.github.com>
2025-07-08 10:57:36 +0900

    feat: add join-game flow

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -6,6 +6,7 @@ import Register from '../views/Register.vue'
 import UserProfile from '../views/UserProfile.vue'
 import Messages from '../views/Messages.vue'
 import History from '../views/History.vue'
+import GameConfig from '../views/GameConfig.vue'
 
 const routes = [
   { path: '/', component: Home },               // 首页
@@ -13,6 +14,7 @@ const routes = [
   { path: '/register', component: Register },   // 注册
   { path: '/rooms', component: Room },          // 房间大厅/游戏入口
   { path: '/room/:id', component: Room },       // 游戏房间
+  { path: '/game-config/:id', component: GameConfig }, // 游戏配置
   { path: '/user', component: UserProfile },    // 个人信息
   { path: '/messages', component: Messages },   // 消息系统
   { path: '/history', component: History }      // 历史对局
---
+++ frontend/src/views/GameConfig.vue
@@ -0,0 +1,33 @@
+<template>
+  <el-card>
+    <h2>游戏配置</h2>
+    <p>你已加入房间 {{ roomId }} ，请在下方选择初始设置。</p>
+    <el-form :model="form" label-width="80px">
+      <el-form-item label="昵称">
+        <el-input v-model="form.nickname" />
+      </el-form-item>
+      <el-form-item label="性别">
+        <el-radio-group v-model="form.gender">
+          <el-radio label="m">男</el-radio>
+          <el-radio label="f">女</el-radio>
+        </el-radio-group>
+      </el-form-item>
+    </el-form>
+    <el-button type="primary" @click="startGame">开始游戏</el-button>
+  </el-card>
+</template>
+
+<script setup>
+import { reactive } from 'vue'
+import { useRoute, useRouter } from 'vue-router'
+
+const route = useRoute()
+const router = useRouter()
+const roomId = route.params.id
+
+const form = reactive({ nickname: '', gender: 'm' })
+
+function startGame() {
+  router.push(`/room/${roomId}`)
+}
+</script>
